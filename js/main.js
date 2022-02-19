530ced732bf5fcc31250712c0ed328079972bc0c
Anatoly U <korvet07@yandex.ru>
2022-02-20 00:35:05 +0300

    выполняет д/з Нужно больше функций

---
+++ js/main.js
@@ -0,0 +1,21 @@
+function getRandomIntInclusive(min, max) {
+  if ( min > max) {
+    return !1;
+  }
+  if (min < 0 || max < 0) {
+    return !1;
+  }
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+getRandomIntInclusive(5.7, 9);
+
+function checkLengthString(checkString, maxLength = 140) {
+  if (checkString.length <= maxLength) {
+    return !0;
+  }
+  return !1;
+}
+checkLengthString('длина строки');
