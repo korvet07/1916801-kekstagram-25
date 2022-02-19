46e48e8794fb61fb33dc078c3c8c93a1d34207b2
Anatoly U <korvet07@yandex.ru>
2022-02-20 00:45:09 +0300

    выполняет д/з Нужно больше функций

--- js/main.js
+++ js/main.js
@@ -1,10 +1,8 @@
 function getRandomIntInclusive(min, max) {
-  if ( min > max) {
+  if (( min > max) || (min < 0 || max < 0)){
     return !1;
   }
-  if (min < 0 || max < 0) {
-    return !1;
-  }
+
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
