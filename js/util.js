b641fbb92ddd904eaae8cc3d489b78e91a973e7b
Anatoly U <korvet07@yandex.ru>
2022-03-11 15:07:02 +0300

    исправление ошибок

--- js/util.js
+++ js/util.js
@@ -1,24 +1,33 @@
 function getRandomIntInclusive(min, max) {
   if ((min >= max) || (min < 0 || max < 0)) {
-    return 0;
+    return null;
   }
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
 function checkLengthString(checkString, maxLength = 140) {
   return checkString.length <= maxLength;
 }
 checkLengthString('строка');
-const getNextRandomInt = (startNumber = 1, maxStep = 1) => {
-  let currentNumber = startNumber;
-  return () => {
-    const resultNumber = currentNumber;
-    currentNumber += getRandomIntInclusive(1, maxStep);
-    return resultNumber;
-  };
-};
 
-export const getNextCommentId = getNextRandomInt(3, 2);
+function createRandomCommentId (min, max) {
+  const previousValues = [];
+  return function () {
+    let currentValue = getRandomIntInclusive(min, max);
+    if (previousValues.length >= (max - min + 1)) {
+      window.console.error(`Перебраны все числа из диапазона от ${ min } до ${ max }`);
+      return null;
+    }
+    while (previousValues.includes(currentValue)) {
+      currentValue = getRandomIntInclusive(min, max);
+    }
+    previousValues.push(currentValue);
+    return currentValue;
+  };
+}
+
+export const getNextCommentId = createRandomCommentId (1, 150);
 export const getRandomArrayElement = (element) => element[getRandomIntInclusive(0, element.length - 1)];
 export {getRandomIntInclusive};
