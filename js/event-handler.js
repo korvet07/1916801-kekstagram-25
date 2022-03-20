2f0906beec23ffad1f0e8dae535173f74d37bbbe
Anatoly U <korvet07@yandex.ru>
2022-03-20 17:26:17 +0300

    добавляет Отрисуй меня полностью (часть 2)

---
+++ js/event-handler.js
@@ -0,0 +1,27 @@
+export const openBigPhoto = (photo) => {
+  photo.classList.remove('hidden');
+  document.querySelector('.social__comment-count').classList.add('hidden');
+  document.querySelector('.comments-loader').classList.add('hidden');
+  document.body.classList.add('modal-open');
+};
+export const сloseBigPhotoEsc = (photo) => {
+  if (!photo.matches('hidden')) {
+    document.addEventListener('keydown', (evt) => {
+      if (evt.key === 'Escape') {
+        evt.preventDefault();
+        photo.classList.add('hidden');
+        document.querySelector('.social__comment-count').classList.remove('hidden');
+        document.querySelector('.comments-loader').classList.remove('hidden');
+        document.body.classList.remove('modal-open');
+      }
+    });
+  }
+};
+export const сloseBigPhotoButton = (photo) => {
+  photo.querySelector('#picture-cancel').addEventListener('click', () => {
+    photo.classList.add('hidden');
+    document.querySelector('.social__comment-count').classList.remove('hidden');
+    document.querySelector('.comments-loader').classList.remove('hidden');
+    document.body.classList.remove('modal-open');
+  });
+};
