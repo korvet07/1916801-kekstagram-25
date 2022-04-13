ea539c245fb501af9d80dc9ba5a4f3b8b7c8d1ba
Anatoly U <korvet07@yandex.ru>
2022-04-13 11:28:57 +0300

    исправляет Перламутровые пуговицы

--- js/filters.js
+++ js/filters.js
@@ -7,14 +7,6 @@ const buttonDiscussed = document.querySelector('#filter-discussed');
 window.onload = () => {
   document.querySelector('.img-filters').classList.remove('img-filters--inactive');
 };
-
-export const debounce = (callback, timeoutDelay) => {
-  let timeoutId;
-  return (...rest) => {
-    clearTimeout(timeoutId);
-    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
-  };
-};
 const onButtonClick = (evt) => {
   document.querySelectorAll('.img-filters__button').forEach((button) => button.classList.remove('img-filters__button--active'));
   evt.target.classList.add('img-filters__button--active');
@@ -22,24 +14,28 @@ const onButtonClick = (evt) => {
 };
 export const onAlternativeRenders = (data) => {
   buttonDefault.addEventListener('click', (evt) => {
-    onButtonClick(evt);
-    debounce(renderPhotos(data.slice()), RERENDER_DELAY );
+    setTimeout(() => {
+      onButtonClick(evt);
+      renderPhotos(data.slice());
+    }, RERENDER_DELAY);
   });
   buttonRandom.addEventListener('click', (evt) => {
-    onButtonClick(evt);
-    const randomPhotos = data.slice().sort(() => Math.random() - 0.5).slice(0, SHOW_RANDOM_PHOTOS);
-    renderPhotos(randomPhotos);
+    setTimeout(() => {
+      onButtonClick(evt);
+      const randomPhotos = data.slice().sort(() => Math.random() - 0.5).slice(0, SHOW_RANDOM_PHOTOS);
+      renderPhotos(randomPhotos);
+    }, RERENDER_DELAY);
   });
   buttonDiscussed.addEventListener('click', (evt) => {
-    onButtonClick(evt);
-    const discussedPhotos = data.slice().sort((a, b) => {
-      if (a.comments === b.comments) {
-        return 0;
-      }
-      return a.comments < b.comments ? 1 : -1;
-    });
-    renderPhotos(discussedPhotos);
+    setTimeout(() => {
+      onButtonClick(evt);
+      const discussedPhotos = data.slice().sort((a, b) => {
+        if (a.comments === b.comments) {
+          return 0;
+        }
+        return a.comments < b.comments ? 1 : -1;
+      });
+      renderPhotos(discussedPhotos);
+    }, RERENDER_DELAY);
   });
-
 };
-
