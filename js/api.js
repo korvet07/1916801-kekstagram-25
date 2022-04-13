35c1a0daa62804fed7436af0f577091fde4393f9
Anatoly U <korvet07@yandex.ru>
2022-04-13 10:23:57 +0300

    добавляет Перламутровые пуговицы

--- js/api.js
+++ js/api.js
@@ -1,37 +1,10 @@
-import { onRenderRandomPhotos, onRenderDefaultPhotos, onRenderDiscussedPhotos, debounce } from './filters.js';
-const RERENDER_DELAY = 500;
-export const getData = (onSuccess, onError) => {
+export const getData = (onSuccess, onError, onAlternativeRenders) => {
   fetch('https://25.javascript.pages.academy/kekstagram/data')
     .then((response) => response.json())
     .then((data) => {
-      const allPhotos = data.slice();
-      onSuccess(allPhotos);
-      onRenderDefaultPhotos(debounce(() => {
-        document.querySelectorAll('.picture').forEach((picture) => { picture.remove(); });
-        onSuccess(allPhotos);
-      }, RERENDER_DELAY));
-      return data;
+      onAlternativeRenders(data);
+      onSuccess(data);
     })
-    .then((data) => {
-      onRenderDiscussedPhotos(debounce(() => {
-        document.querySelectorAll('.picture').forEach((picture) => { picture.remove(); });
-        const discussedPhotos = data.slice().sort((a, b) => {
-          if (a.likes === b.likes) {
-            return 0;
-          }
-          return a.likes < b.likes ? 1 : -1;
-        });
-        onSuccess(discussedPhotos);
-      }, RERENDER_DELAY));
-      return data;
-    })
-    .then((photos) => onRenderRandomPhotos(debounce(() => {
-      window.console.log(photos);
-      document.querySelectorAll('.picture').forEach((picture) => { picture.remove(); });
-      const randomPhotos = photos.slice().sort(() => Math.random() - 0.5).slice(0, 10);
-      window.console.log(randomPhotos);
-      onSuccess(randomPhotos);
-    }, RERENDER_DELAY)))
     .catch(() => onError('Ошибка загрузки фото, перезагрузите страницу!'));
 };
 export const sendData = (body, onSendStatus, onFinally, onMoreAction, onSomeMoreAction) => {
--- js/filters.js
+++ js/filters.js
@@ -1,9 +1,13 @@
+import { renderPhotos } from './render-photos.js';
+const SHOW_RANDOM_PHOTOS = 10;
+const RERENDER_DELAY = 500;
 const buttonDefault = document.querySelector('#filter-default');
 const buttonRandom = document.querySelector('#filter-random');
 const buttonDiscussed = document.querySelector('#filter-discussed');
 window.onload = () => {
   document.querySelector('.img-filters').classList.remove('img-filters--inactive');
 };
+
 export const debounce = (callback, timeoutDelay) => {
   let timeoutId;
   return (...rest) => {
@@ -11,24 +15,31 @@ export const debounce = (callback, timeoutDelay) => {
     timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
   };
 };
-const onButtonClick = (evt, cb) => {
+const onButtonClick = (evt) => {
   document.querySelectorAll('.img-filters__button').forEach((button) => button.classList.remove('img-filters__button--active'));
   evt.target.classList.add('img-filters__button--active');
-  return cb();
+  document.querySelectorAll('.picture').forEach((picture) => picture.remove());
+};
+export const onAlternativeRenders = (data) => {
+  buttonDefault.addEventListener('click', (evt) => {
+    onButtonClick(evt);
+    debounce(renderPhotos(data.slice()), RERENDER_DELAY );
+  });
+  buttonRandom.addEventListener('click', (evt) => {
+    onButtonClick(evt);
+    const randomPhotos = data.slice().sort(() => Math.random() - 0.5).slice(0, SHOW_RANDOM_PHOTOS);
+    renderPhotos(randomPhotos);
+  });
+  buttonDiscussed.addEventListener('click', (evt) => {
+    onButtonClick(evt);
+    const discussedPhotos = data.slice().sort((a, b) => {
+      if (a.comments === b.comments) {
+        return 0;
+      }
+      return a.comments < b.comments ? 1 : -1;
+    });
+    renderPhotos(discussedPhotos);
+  });
+
 };
 
-export const onRenderRandomPhotos = (cb) => {
-  buttonRandom.addEventListener('click', (evt) => {
-    onButtonClick(evt, cb);
-  });
-};
-export const onRenderDefaultPhotos = (cb) => {
-  buttonDefault.addEventListener('click', (evt) => {
-    onButtonClick(evt, cb);
-  });
-};
-export const onRenderDiscussedPhotos = (cb) => {
-  buttonDiscussed.addEventListener('click', (evt) => {
-    onButtonClick(evt, cb);
-  });
-};
